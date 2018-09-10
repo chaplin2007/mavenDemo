6e78a136f07e896e0a2cc76e76b6866b190d7bbe
chaplin2007 <41979802+chaplin2007@users.noreply.github.com>
2018-09-10 21:00:26 +0900

    aa

---
+++ gitTest2/src/gitTest/Main.java
@@ -0,0 +1,13 @@
+package gitTest;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a_a = "hahahaha";
+		
+		System.out.println("Hello_world"+a_a);
+		System.out.println("Hello_world 2 "+a_a);
+	}
+	
+}
